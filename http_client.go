91fac3943d14341dfb8b010660b7cd01dd1c4847
Rodrigo Sampaio Vaz <rodrigo.vaz@gmail.com>
2013-06-12 18:10:06 -0300

    json decoding and items filter

--- http_client.go
+++ http_client.go
@@ -4,40 +4,90 @@ import (
 	"fmt"
 	"net/http"
 	"io/ioutil"
+	"encoding/json"
 )
 
 type Client struct {
-	username string
-	password string
-	client *http.Client
+	Username string
+	Password string
+	Client *http.Client
+	
+}
+
+type Product struct {
+	Name string
+	Id int64
+}
+
+type Item struct {
+	Who string
+	What string
+	Why string
+	Tags []string
+	Assigned_to string
 }
 
 func NewClient(user, pass string) (*Client) {
 	c := new(Client)
-	c.username = user
-	c.password = pass
-	c.client = &http.Client{}
+	c.Username = user
+	c.Password = pass
+	c.Client = &http.Client{}
 	return c
 }
 
-func (h *Client) Products() (string, error) {
-	req, err := http.NewRequest("GET", "https://sprint.ly/api/products.json", nil)
+func (h *Client) HTTPRequest(method string, url string) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, nil)
 
 	if err != nil {
 		fmt.Println("request failed")
 	}
-
-	req.SetBasicAuth(h.username, h.password)
-	resp, err := h.client.Do(req)
+	
+	req.SetBasicAuth(h.Username, h.Password)
+	resp, err := h.Client.Do(req)
 
 	if err != nil {
-		return "failed", err
+		return nil, err
 	}
+	
+	return resp, nil
+}
 
+func (h *Client) Products() ([]Product, error) {
+	resp, _ := h.HTTPRequest("GET", "https://sprint.ly/api/products.json")
 	body, err := ioutil.ReadAll(resp.Body)
 	
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(body), nil
+
+	var products []Product
+
+	err = json.Unmarshal(body, &products)
+
+	if err != nil {
+		fmt.Println("json", err)
+	}
+	return products, nil
 }
+
+func (h *Client) Items(product_id int64) ([]Item, error) {
+	var items []Item
+
+	resp, err := h.HTTPRequest("GET", fmt.Sprintf("https://sprint.ly/api/products/%d/items.json?type=story", product_id))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	
+	err = json.Unmarshal(body, &items)
+	if err != nil {
+		return nil, err
+	}
+
+	return items, nil;
+}
+
--- sprintly_reports/main.go
+++ sprintly_reports/main.go
@@ -8,6 +8,24 @@ import (
 
 func main() {
 	client := sprintly_reports.NewClient(os.Args[1], os.Args[2])
+	
 	resp, _ := client.Products()
-	fmt.Println(resp)
+	
+	items, _ := client.Items(resp[0].Id)
+
+	fmt.Println(items[0].What)
+
+	for _, v := range items {
+		fmt.Println(v.Tags)
+	}
 }
+
+
+
+
+
+
+
+
+
+
